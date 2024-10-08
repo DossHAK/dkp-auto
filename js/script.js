1f7985c6f6cefdcdee5bc9852a56e2772b9b1e44
DossHAK <da.vlad.at@gmail.com>
2024-10-08 16:42:49 +0800

    2024-10-08

--- js/script.js
+++ js/script.js
@@ -12,7 +12,39 @@ function getFormValue(event) {
     pasport_pro_s = form.querySelector('[id="pasport_pro_s"]'),
     pasport_pro_n = form.querySelector('[id="pasport_pro_n"]'),
     pasport_pro_kod = form.querySelector('[id="pasport_pro_kod"]'),
-    pasport_pro_date = form.querySelector('[id="pasport_pro_date"]');
+    pasport_pro_date = form.querySelector('[id="pasport_pro_date"]'),
+    
+    fio_po = form.querySelector('[id="fio_po"]'),
+    date_birth_po = form.querySelector('[id="date_birth_po"]'),
+    pasport_po_s = form.querySelector('[id="pasport_po_s"]'),
+    pasport_po_n = form.querySelector('[id="pasport_po_n"]'),
+    pasport_po_kod = form.querySelector('[id="pasport_po_kod"]'),
+    pasport_po_reg = form.querySelector('[id="pasport_po_reg"]'),
+    
+    model = form.querySelector('[id="model"]'),
+    vin = form.querySelector('[id="vin"]'),
+    type = form.querySelector('[id="type"]'),
+    year = form.querySelector('[id="year"]'),
+    engine_m = form.querySelector('[id="engine_m"]'),
+    engine_n = form.querySelector('[id="engine_n"]'),
+    frame = form.querySelector('[id="frame"]'),
+    body = form.querySelector('[id="body"]'),
+    km = form.querySelector('[id="km"]'),
+    color = form.querySelector('[id="color"]'),
+
+    pts_s = form.querySelector('[id="pts_s"]'),
+    pts_n = form.querySelector('[id="pts_n"]'),
+    pts_date = form.querySelector('[id="pts_date"]'),
+    pts_reg = form.querySelector('[id="pts_reg"]'),
+    
+    sts_s = form.querySelector('[id="pts_s"]'),
+    sts_n = form.querySelector('[id="sts_n"]'),
+    sts_date = form.querySelector('[id="sts_date"]'),
+    sts_reg = form.querySelector('[id="sts_reg"]'),
+    
+    number = form.querySelector('[id="number"]'),
+    price = form.querySelector('[id="price"]');
+
 
     const data = {
         date: date.value,
